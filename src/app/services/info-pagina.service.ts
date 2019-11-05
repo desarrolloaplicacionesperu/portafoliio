686cfca0bf722ae9eed31428783b0e1c806ffa29
Joe Jordan <ioexiordan@gmail.com>
2019-11-05 04:35:11 -0500

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { InfoPagina } from '../interfaces/info-pagina.interface';
+import { InfoEquipo } from '../interfaces/info-equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +10,48 @@ export class InfoPaginaService {
 
   // info: any = {};
   info: InfoPagina = {};
+  equipo: any[] = []; // equipo: InfoEquipo = {};
   cargada = false;
+  equipo2: InfoEquipo[] = [];
 
-  constructor( private http: HttpClient) {
-    console.log('Servicio de infoPagina listo');
+  constructor(private http: HttpClient) {
 
+    this.CargarInfo();
+    this.CargarEquipo();
+    this.CargarEquipo2();
+
+  }
+
+  private CargarInfo() {
     // Leer el archivo json
     this.http
-    .get('assets/data/data-pagina.json')
-    .subscribe( (resp: InfoPagina) => {
+      .get('assets/data/data-pagina.json')
+      .subscribe((resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      });
+  }
+
+  private CargarEquipo() {
+    // Leer json desde firebase
+    this.cargada = false;
+    this.http
+    .get('https://angular-html-d44f8.firebaseio.com/equipo.json')
+    .subscribe((resp: any[]) => {
       this.cargada = true;
-      this.info = resp;
-      console.log(resp);
+      this.equipo = resp;
     });
   }
+
+  private CargarEquipo2() {
+    // Leer json desde firebase
+    this.cargada = false;
+    this.http
+    .get('https://angular-html-d44f8.firebaseio.com/equipo.json')
+    .subscribe((resp: InfoEquipo[]) => {
+      this.cargada = true;
+      this.equipo2 = resp;
+    });
+  }
+
 }
